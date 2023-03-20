a7399bfacde660cd9f5c47f9a947d5efa7b8aba8
marcgumo <58789378+marcgumo@users.noreply.github.com>
2023-03-20 16:54:24 +0100

    Initial commit

---
+++ Assets/Scripts/Manager/JoinRoomController.cs
@@ -0,0 +1,17 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinRoomController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Manager/JoinServerController.cs
@@ -0,0 +1,25 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JoinServerController : MonoBehaviourPunCallbacks
+{
+    
+    
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("LobbyScene");
+    }
+}
