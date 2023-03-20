5a50477e6e0eab7c93a0566b0dd2f805a28cab15
marcgumo <58789378+marcgumo@users.noreply.github.com>
2023-03-20 16:58:08 +0100

    Join room

--- Assets/Scripts/Manager/JoinRoomController.cs
+++ Assets/Scripts/Manager/JoinRoomController.cs
@@ -1,17 +1,26 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class JoinRoomController : MonoBehaviourPunCallbacks
 {
-    void Start()
+    [SerializeField] private TMP_InputField createInput;
+    [SerializeField] private TMP_InputField joinInput;
+
+    public void CreateRoom()
     {
-        
+        PhotonNetwork.CreateRoom(createInput.text);
     }
 
-    void Update()
+    public void JoinRoom()
     {
-        
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("GameScene");
     }
 }
